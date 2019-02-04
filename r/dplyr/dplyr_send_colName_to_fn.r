e1404e4818a705f438d4da59df821fbe89d40a3c
elsaduplessis <elsadp.za@gmail.com>
2019-02-04 16:39:41 +0100

    Commitment uno

---
+++ r/dplyr/dplyr_send_colName_to_fn.r
@@ -0,0 +1,70 @@
+
+### *A solution presents itself:
+
+require( dplyr )
+require( ggplot2 )
+
+# *1. Test simple function =============================
+fn.test <-  function( DF, COLNAME ) {
+    tmp.name    <-  enquo( COLNAME )
+
+    df.out  <-  DF %>%
+                dplyr::select( !!tmp.name )
+                
+    return ( df.out )
+}
+
+df.tmp  <-  data.frame( colA = 1:4, columnOddName = c("A", "h", "o", "y"), colB = 5:8 )
+
+fn.test( df.tmp, columnOddName )
+# -----------------------------------------------------
+
+
+# *2. Test with a ggplot ==============================
+fn.test.fig <-  function( DF, COLA, COLB ) {
+    tmp.name.a  <-  enquo( COLA )
+    tmp.name.b  <-  enquo( COLB )
+
+    p.out   <-  DF %>%
+                ggplot( aes(x = !!tmp.name.a, y = !!tmp.name.b) ) + 
+                geom_point( colour = "#cc00cc", size = 2 ) + 
+                labs( title = "success" )
+                
+    return ( p.out )
+}
+
+fn.test.fig( df.tmp, colA, colB )
+# -----------------------------------------------------
+
+
+
+# *SOURCE of WISDOM =============================
+
+# See last comment with latest dplyr updates:
+# https://stackoverflow.com/questions/28973056/in-r-pass-column-name-as-argument-and-use-it-in-function-with-dplyrmutate-a
+
+        # With the devel version of dplyr (0.5.0) or in the new version (0.6.0 - awaiting release in April 2017), this can be done using slightly different syntax
+
+        # library(dplyr)
+        # funcN <- function(dat, varname){
+        #  expr <- enquo(varname)
+        #  dat %>%
+        #      mutate(v3 = sum(!!expr))
+        #      #or
+        #      #mutate(v3 = sum(UQ(expr)))
+
+        # } 
+
+        # funcN(data, v1)
+        # #  v1 v2 v3
+        # #1  1  3  3
+        # #2  2  4  3
+
+        # Here, enquo takes the arguments and returns the value as a quosure (similar to substitute in base R) by evaluating the function arguments lazily and inside the summarise, we ask it to unquote (!! or UQ) so that it gets evaluated.
+        # shareimprove this answer
+        # answered Apr 14 '17 at 20:17
+        # akrun
+        # 405k13196269
+
+
+
---
+++ r/outliers/winsor_with_mad.r
@@ -0,0 +1,46 @@
+
+
+
+# Outliers / extreme values: Winsorization with MAD setting the threshold, instead of quantile %'s.
+fn_winsor_mad   <-  function ( xvec, madDeviations ) {
+            # Set number of MAD deviations to denote extreme values
+            madLimit    = mad( xvec, na.rm = TRUE ) * madDeviations
+            
+            upperLim    = median( xvec, na.rm = TRUE ) + madLimit
+            lowerLim    = median( xvec, na.rm = TRUE ) - madLimit
+            
+            # Winsorisation: REPLACE these values with the appropriate limit
+            #               ( trimming implies removal, not replacement )
+            xvec[xvec > upperLim]   = upperLim
+            xvec[xvec < lowerLim]   = lowerLim  
+
+            return (xvec)
+} 
+
+
+
+# https://en.wikipedia.org/wiki/Winsorizing
+
+# Note that winsorizing is not equivalent to simply excluding data, which is a simpler procedure, called trimming or truncation, 
+# but is a method of censoring data.
+# In a trimmed estimator, the extreme values are discarded; in a winsorized estimator, 
+# the extreme values are instead replaced by certain percentiles (the trimmed minimum and maximum). 
+
+
+
+
+# https://en.wikipedia.org/wiki/Median_absolute_deviation
+
+# In statistics, the median absolute deviation (MAD) is a robust measure of the variability of a univariate sample of quantitative data. 
+# It can also refer to the population parameter that is estimated by the MAD calculated from a sample. 
+
+# The median absolute deviation is a measure of statistical dispersion. Moreover, the MAD is a robust statistic, being more resilient to outliers 
+# in a data set than the standard deviation. 
+# In the standard deviation, the distances from the mean are squared, so large deviations are weighted more heavily, 
+# and thus outliers can heavily influence it. In the MAD, the deviations of a small number of outliers are irrelevant.
+
+# Because the MAD is a more robust estimator of scale than the sample variance or standard deviation, 
+# it works better with distributions without a mean or variance, such as the Cauchy distribution. 
+
+# See also:
+# https://eurekastatistics.com/using-the-median-absolute-deviation-to-find-outliers/
